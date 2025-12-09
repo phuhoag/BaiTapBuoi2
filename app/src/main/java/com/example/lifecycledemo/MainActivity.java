e1396e936962090d8e172d7265772c88170a96fe
phuhoag <11a8hoangkimquyphu@gmail.com>
2025-12-09 16:18:47 +0700

    updatebai2

--- app/src/main/java/com/example/lifecycledemo/MainActivity.java
+++ app/src/main/java/com/example/lifecycledemo/MainActivity.java
@@ -3,14 +3,39 @@ package com.example.lifecycledemo;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.content.Intent;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnExplicit, btnImplicit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Lifecycle", "Hàm onCreate được gọi");
+
+        // Ánh xạ 2 nút
+        btnExplicit = findViewById(R.id.btnExplicit);
+        btnImplicit = findViewById(R.id.btnImplicit);
+
+        // ============================
+        // EXPLICIT INTENT
+        // ============================
+        btnExplicit.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        });
+
+        // ============================
+        // IMPLICIT INTENT
+        // ============================
+        btnImplicit.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://google.com"));
+            startActivity(intent);
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/lifecycledemo/SecondActivity.java
@@ -0,0 +1,12 @@
+package com.example.lifecycledemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
